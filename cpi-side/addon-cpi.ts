7bc8aee9839b957f539fadb5c02dbf0a70f4eb3c
Noam Ben David <noam.b@pepperi.com>
2022-08-10 11:42:41 +0300

    fix typo

--- cpi-side/addon-cpi.ts
+++ cpi-side/addon-cpi.ts
@@ -1,7 +1,7 @@
 import '@pepperi-addons/cpi-node'
 
 export async function load(configuration: any) {
-    pepperi.events.intercept("PushNotificationRecieved" as any, {}, async (data, next) => {
+    pepperi.events.intercept("PushNotificationReceived" as any, {}, async (data, next) => {
         const main = async (data) => {
             const page = await pepperi.slugs.getPage("/notifications");
             if (page.success) {
