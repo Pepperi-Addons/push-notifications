18704c099598ffc7c31885de52cc0e18940e0233
Noam Ben David <noam.b@pepperi.com>
2022-09-14 14:25:39 +0300

    DI-21072 - Linked Object - Sync

--- cpi-side/addon-cpi.ts
+++ cpi-side/addon-cpi.ts
@@ -1,15 +1,46 @@
 import '@pepperi-addons/cpi-node'
+import { Client } from '@pepperi-addons/cpi-node/build/cpi-side/events';
 
 export async function load(configuration: any) {
     pepperi.events.intercept("PushNotificationReceived" as any, {}, async (data, next) => {
         const main = async (data) => {
-            const page = await pepperi.slugs.getPage("/notifications");
-            if (page.success) {
-                data.client?.navigateTo({ url: '/notifications'});
-            } else {
-                data.client?.navigateTo({ url: '/homepage' });
+            console.log("Received notification:" ,data.Notification)
+            if (data.Notification?.NavigationPath != undefined) {
+                let res = await sync(data.client);
+                if (res.success == true) {
+                    data.client?.navigateTo({
+                        url: data.Notification?.NavigationPath,
+                        history: "ClearAll",
+                      });
+                }
+                else {
+                    console.log("sync action failed")
+                }
+            }
+            else {
+               await navigateToNotificationsSlug(data);
             }
         };
         await next(main)  
     });
+}
+
+async function navigateToNotificationsSlug(data: any) {
+    // default navigation
+    let url = '/homepage';
+    const page = await pepperi.slugs.getPage("/notifications");
+    if (page.success) {
+        url = '/notifications'
+    }
+      data.client?.navigateTo({
+        url: url,
+        history: "ClearAll",
+      });
+}
+
+async function sync(client: Client) {
+    const syncOptions = {    
+        "allowContinueInBackground": false
+    };
+    return await client["sync"](syncOptions);
 }
