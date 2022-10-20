52289043504852c1bfee21369a6acb626fed8828
Noam Ben David <noam.b@pepperi.com>
2022-10-20 11:31:05 +0300

    change the signature of getNotifications in notifications service

--- client-side/src/app/services/notifications.services.ts
+++ client-side/src/app/services/notifications.services.ts
@@ -12,10 +12,10 @@ export class NotificationsService {
 
     }
 
-    getNotifications(query?: string) {
+    getNotifications(whereClause?: string) {
         let url = `/addons/api/${this.addonService.addonUUID}/api/notifications`
-        if (query) {
-            url = url + query;
+        if (whereClause) {
+            url = url + '?where=' + whereClause;
         }
         return this.addonService.pepGet(encodeURI(url)).toPromise();
     }
